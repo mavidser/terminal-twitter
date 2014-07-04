8a42bf18716969db96a15cfe7c27cf3f82c46596
mavidser <mavidser@gmail.com>
2014-07-04 12:04:46 +0530

    functions

--- app.py
+++ app.py
@@ -3,15 +3,12 @@ from keys import *
 import webbrowser
 import tweepy
 import cPickle
+import click
 
 USER_FILE = 'user.pkl'
-try:
-	with open(USER_FILE, 'r') as f:
-	  access_token = cPickle.load(f)
-	  auth = tweepy.OAuthHandler(API_KEY, API_SECRET,'https://sidverma.net/tt/callback')
-	  auth.set_access_token(access_token.key, access_token.secret)
-except:
-	auth = tweepy.OAuthHandler(API_KEY, API_SECRET,'https://sidverma.net/tt/callback')
+
+def save_user():
+	auth = tweepy.OAuthHandler(API_KEY, API_SECRET,'http://sidverma.net/tt/callback')
 	try:
 	  redirect_url = auth.get_authorization_url()
 	except tweepy.TweepError:
@@ -24,5 +21,18 @@ except:
 	    cPickle.dump(auth.access_token, f, cPickle.HIGHEST_PROTOCOL)
 	except tweepy.TweepError:
 	  print 'Error! Failed to get access token.'
+	return auth
 
-api = tweepy.API(auth)
+def main():
+	try:
+		with open(USER_FILE, 'r') as f:
+		  access_token = cPickle.load(f)
+		  auth = tweepy.OAuthHandler(API_KEY, API_SECRET,'http://sidverma.net/tt/callback')
+		  auth.set_access_token(access_token.key, access_token.secret)
+	except:
+		auth = save_user()
+	api = tweepy.API(auth)
+
+if __name__ == '__main__':
+  main()
+  
