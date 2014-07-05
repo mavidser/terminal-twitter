55f75c0c86ee54db17f1295b057981a4d8db0e15
mavidser <mavidser@gmail.com>
2014-07-05 17:12:47 +0530

    add keys.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
   author='Sid Verma',
   author_email='sid@sidverma.net',
   url='http://github.com/mavidser/tt',
-  scripts=['app.py'],
+  scripts=['app.py','keys.py'],
   install_requires=[
     'click==2.4',
     'tweepy==2.3.0'
